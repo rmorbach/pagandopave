332eab703da9c3e33430f038ca5e88f5eb17b3a7
kpereira <kpereira@cpqd.com.br>
2017-08-26 18:13:28 -0300

    persistence

---
+++ GatewayApi/gateway/Gateway-ejb/src/main/java/br/com/pagandopave/api/UsuarioManager.java
@@ -0,0 +1,15 @@
+package br.com.pagandopave.api;
+
+import javax.ejb.Local;
+
+import br.com.pagandopave.entidade.Usuario;
+
+@Local
+public interface UsuarioManager {
+	
+	public void cadastrar(Usuario usuario);
+	public Usuario alterar(Usuario usuario);
+	public void deletar(Usuario usuario);
+	public Usuario consultarPorNickName(String nickName);
+
+}
